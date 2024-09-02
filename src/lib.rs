54377f190bebc80c06b20eb3b85206a78bb28e18
lysolaka <lysolaka@gmail.com>
2024-09-02 14:34:39 +0200

    Added helper functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 #[derive(Debug)]
 pub struct Config {
     pub chars: bool,
@@ -26,4 +28,72 @@ impl Config {
 
         Ok(Config { chars, file_path })
     }
+}
+
+pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
+
+fn count_lines(contents: &str) -> usize {
+    contents.lines().count()
+}
+
+fn count_words(contents: &str) -> usize {
+    contents.split_whitespace().count()
+}
+
+fn count_bytes(contents: &str) -> usize {
+    contents.bytes().count()
+}
+
+fn count_chars(contents: &str) -> usize {
+    contents.chars().count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn line_count() {
+        let text = "\
+        Some random text,
+        needs a few lines,
+        hopefully 3 is enough,
+        nevermind, I'll put 4 for
+        SaFeTy, ohhh... it's 5 now";
+
+        assert_eq!(count_lines(text), 5);
+    }
+
+    #[test]
+    fn word_count() {
+        let text = "\
+        Some random text,
+        needs a few lines,
+        hopefully 3 is enough,
+        nevermind, I'll put 4 for
+        SaFeTy, ohhh... it's 5 now";
+
+        assert_eq!(count_words(text), 21);
+    }
+
+    #[test]
+    fn byte_count() {
+        let s1 = "haha ascii only";
+        let s2 = "hąhą ńó ąśćii";
+
+        assert_eq!(count_bytes(s1), 15);
+        assert_eq!(count_bytes(s2), 20);
+    }
+
+    #[test]
+    fn char_count() {
+        let s1 = "haha ascii only";
+        let s2 = "hąhą ńó ąśćii";
+
+        assert_eq!(count_chars(s1), 15);
+        assert_eq!(count_chars(s2), 13);       
+    }
+
 }
