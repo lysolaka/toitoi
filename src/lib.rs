715d926d15deb34249a6d164bd95545fb6f013ad
lysolaka <lysolaka@gmail.com>
2024-09-03 10:27:05 +0200

    Added help message

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::{error::Error, fs};
 
 #[derive(Debug)]
 pub struct Config {
+    pub help: bool,
     pub chars: bool,
     pub file_path: String,
 }
@@ -16,7 +17,14 @@ impl Config {
             Some(s) if s == "-c" || s == "--chars" => {
                 arg = args.next();
                 true
-            },
+            }
+            Some(s) if s == "-h" || s == "--help" => {
+                return Ok(Config {
+                    help: true,
+                    chars: false,
+                    file_path: String::from(""),
+                })
+            }
             Some(_) => false,
             None => return Err("Not enough arguments provided!"),
         };
@@ -26,11 +34,23 @@ impl Config {
             None => return Err("No filename provided!"),
         };
 
-        Ok(Config { chars, file_path })
+        Ok(Config {
+            help: false,
+            chars,
+            file_path,
+        })
     }
 }
 
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    if config.help {
+        println!("Usage: toitoi [OPTION] [FILE]");
+        println!("Print line, word, byte count for FILE\n");
+        println!("  -c, --chars  Print the number of characters instead of bytes");
+        println!("  -h, --help   Display this help and exit");
+        return Ok(())
+    }
+
     let contents = fs::read_to_string(&config.file_path)?;
 
     let line_count = count_lines(&contents);
@@ -42,14 +62,14 @@ pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
             println!("Lines: {}", line_count);
             println!("Words: {}", word_count);
             println!("Characters: {}", char_count);
-        },
+        }
         false => {
             let byte_count = count_bytes(&contents);
             println!("File: {}", config.file_path);
             println!("Lines: {}", line_count);
             println!("Words: {}", word_count);
             println!("Bytes: {}", byte_count);
-        },
+        }
     }
     Ok(())
 }
@@ -73,7 +93,7 @@ fn count_chars(contents: &str) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn line_count() {
         let text = "\
@@ -113,7 +133,6 @@ mod tests {
         let s2 = "hąhą ńó ąśćii";
 
         assert_eq!(count_chars(s1), 15);
-        assert_eq!(count_chars(s2), 13);       
+        assert_eq!(count_chars(s2), 13);
     }
-
 }
